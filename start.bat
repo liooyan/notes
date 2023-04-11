27ca37dc01db6187239f27fcc79ded525c44a5d5
李延 <liyan@wondersoft.cn>
2023-04-11 09:56:52 +0800

    up

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 c:                                 
-cd Users\Administrator\Desktop\OneDrive - liyan\笔记                
+cd 'Users\Administrator\Desktop\OneDrive - liyan\notes\'                
 
 git add .                           
 git commit -m "up"       
