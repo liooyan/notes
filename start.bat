8ed3a3e9b64a1e546dff9716e76927efd48ca967
李延 <liyan@wondersoft.cn>
2023-04-11 09:55:26 +0800

    up

---
+++ start.bat
@@ -0,0 +1,7 @@
+c:                                 
+cd Users\Administrator\Desktop\OneDrive - liyan\笔记                
+
+git add .                           
+git commit -m "up"       
+git push -u origin master 
+
