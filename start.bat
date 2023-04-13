8ca6b3c7393baca25ba374dcb94924abd11a2893
李延 <liyan@wondersoft.cn>
2023-04-13 17:10:23 +0800

    取消自动提交github

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-c:                                 
-cd "C:\Users\Administrator\Desktop\OneDrive - liyan\notes"
-
-git add .                           
-git commit -m "up"   
-git pull origin master     
-git push -u origin master 
-
